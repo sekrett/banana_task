d1c112d54d6bfa753ff6113b31f455f69520afef
Alexander Zubkov <info@zubkov.info>
2016-11-28 12:57:54 +0300

    Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def default_url_options
+    if Rails.env.development?
+      { host: 'lvh.me', port: 3000 }
+    elsif Rails.env.production?
+      # TODO
+      raise 'default_url_options not set for production'
+    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
