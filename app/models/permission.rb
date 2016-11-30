8898dc9b4f774d2411538358ebf5b07b7bb7c35f
Alexander Zubkov <info@zubkov.info>
2016-11-30 20:36:12 +0300

    Fix permissions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -11,7 +11,8 @@ class Permission
     can :posts, :create if user
 
     can :comments, :read
-    can :comments, :manage do |comment|
+    can :comments, :create if user
+    can :comments, [:edit, :destroy] do |comment|
       comment.user == user && comment.created_at > COMMENT_MANAGEABLE_INTERVAL.ago
     end
   end
