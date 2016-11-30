0e103cf973f90fba3c5e9f8dade233045aad060f
Alexander Zubkov <info@zubkov.info>
2016-11-30 13:39:04 +0300

    Destroy dependent posts with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+
+  has_many :posts, dependent: :destroy
 end
