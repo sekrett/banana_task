8ce37828fcfb89c9c5b848684e36f8f62931499d
Alexander Zubkov <info@zubkov.info>
2016-12-03 21:34:08 +0300

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :posts do
-    resources :comments, only: [:create, :edit, :update, :destroy]
+    resources :comments, only: [:edit, :update, :destroy]
   end
   get 'tags/:tag', to: 'posts#index', as: :tag
 
